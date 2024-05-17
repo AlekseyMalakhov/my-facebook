c548b2b8ab0b6ba85fb796f402cadf12c766f7a9
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-05-17 15:43:53 +0300

    add footer

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,55 @@
 import { css } from "@emotion/react";
 
-const container = css({});
+const container = css({
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+});
+const main = css({
+    display: "flex",
+    backgroundColor: "#f0f2f5",
+    flex: 1,
+});
+const footer = css({
+    display: "flex",
+    height: "160px",
+    color: "#8a8d91",
+    fontSize: "12px",
+});
+//nth-child(n+1)
+const langsList = css({
+    margin: "0 40px",
+    paddingTop: "20px",
+    "span:nth-child(n+1)": {
+        paddingLeft: "10px",
+    },
+});
+
+const langs = [
+    "English (US)",
+    "Русский",
+    "Беларуская",
+    "العربية",
+    "Türkçe",
+    "Polski",
+    "Italiano",
+    "Tiếng Việt",
+    "Deutsch",
+    "Español",
+    "Português (Brasil)",
+];
 
 export default function Login() {
-    return <div css={container}>Hello it's page</div>;
+    return (
+        <div css={container}>
+            <main css={main}>hi all</main>
+            <footer css={footer}>
+                <div css={langsList}>
+                    {langs.map((item) => (
+                        <span key={item}>{item}</span>
+                    ))}
+                </div>
+            </footer>
+        </div>
+    );
 }
