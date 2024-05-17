ef014e952c6099982dcecb78b39807f56a13a6a9
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-05-17 16:59:58 +0300

    fix lists in footer

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -12,17 +12,28 @@ const main = css({
 });
 const footer = css({
     display: "flex",
+    justifyContent: "center",
+    margin: "0 40px",
+});
+const footerContent = css({
+    display: "flex",
+    flexDirection: "column",
     height: "160px",
     color: "#8a8d91",
     fontSize: "12px",
+    maxWidth: "980px",
 });
-//nth-child(n+1)
 const langsList = css({
-    margin: "0 40px",
     paddingTop: "20px",
-    "span:nth-child(n+1)": {
-        paddingLeft: "10px",
+    span: {
+        paddingRight: "10px",
+        display: "inline-block",
     },
+});
+const line = css({
+    borderBottom: "1px solid #dddfe2",
+    paddingTop: "8px",
+    marginBottom: "8px",
 });
 
 const langs = [
@@ -39,15 +50,62 @@ const langs = [
     "Português (Brasil)",
 ];
 
+const links = [
+    "SignUp",
+    "LogIn",
+    "Messenger",
+    "Facebook Lite",
+    "Video",
+    "Places",
+    "Games",
+    "Marketplace",
+    "Meta Pay",
+    "Meta Store",
+    "Meta Quest",
+    "Meta AI",
+    "Instagram",
+    "Threads",
+    "Fundraisers",
+    "Services",
+    "Voting",
+    "Information Center",
+    "Privacy",
+    "Policy",
+    "Privacy Center",
+    "Groups",
+    "About",
+    "Create ad",
+    "Create Page",
+    "Developers",
+    "Careers",
+    "Cookies",
+    "Ad choices",
+    "Terms",
+    "Help",
+    "Contact Uploading & Non-Users",
+];
+
 export default function Login() {
     return (
         <div css={container}>
             <main css={main}>hi all</main>
             <footer css={footer}>
-                <div css={langsList}>
-                    {langs.map((item) => (
-                        <span key={item}>{item}</span>
-                    ))}
+                <div css={footerContent}>
+                    <div css={langsList}>
+                        {langs.map((item) => (
+                            <span key={item} style={{ lineHeight: 1.6 }}>
+                                {item}
+                            </span>
+                        ))}
+                    </div>
+                    <div css={line}></div>
+                    <div css={langsList}>
+                        {links.map((item) => (
+                            <span key={item} style={{ lineHeight: 1.6 }}>
+                                {item}
+                            </span>
+                        ))}
+                    </div>
                 </div>
             </footer>
         </div>
