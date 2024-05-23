76bed2fe3ca529f323e99998386225356c255ca7
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-05-23 17:11:23 +0300

    show labels correctly

--- src/features/LoginSelector.tsx
+++ src/features/LoginSelector.tsx
@@ -1,26 +1,35 @@
 import { css } from "@emotion/react";
+import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
+import FormControl from "@mui/material/FormControl";
+import { useId } from "react";
 
 type Props = {
     label: string;
-    handleChange: (event: SelectChangeEvent) => void;
-    items: string[];
-    value: string;
+    handleChange: (event: SelectChangeEvent<string | number>) => void;
+    items: string[] | number[];
+    value: string | number;
 };
 
-const container = css({});
+const container = css({
+    marginTop: "10px",
+});
 
 export default function LoginSelector({ label, handleChange, items, value }: Props) {
+    const id = useId();
     return (
         <div css={container}>
-            <Select value={value} label={label} onChange={handleChange} size="small">
-                {items.map((item) => (
-                    <MenuItem value={item} key={item}>
-                        {item}
-                    </MenuItem>
-                ))}
-            </Select>
+            <FormControl size="small">
+                <InputLabel id={id}>{label}</InputLabel>
+                <Select value={value} label={label} labelId={id} onChange={handleChange} size="small" style={{ width: "125px" }}>
+                    {items.map((item) => (
+                        <MenuItem value={item} key={item}>
+                            {item}
+                        </MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
         </div>
     );
 }
--- src/widgets/SignUpForm.tsx
+++ src/widgets/SignUpForm.tsx
@@ -34,15 +34,58 @@ const form = css({
     padding: "16px",
 });
 
-const birthday = css({});
+const birthday = css({
+    marginTop: "10px",
+});
+
+const title = css({
+    fontSize: "12px",
+    color: "#606770",
+    lineHeight: "20px",
+});
+
+const selectors = css({
+    display: "flex",
+    justifyContent: "space-between",
+});
 
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const days: number[] = [];
+for (let i = 1; i < 31; i++) {
+    days.push(i);
+}
+
+const years: number[] = [];
+const yearNow = new Date(Date.now()).getFullYear();
+
+for (let i = 1940; i <= yearNow; i++) {
+    years.push(i);
+}
+years.reverse();
 
 export default function SignUpForm({ handleClose, open }: Props) {
     const [month, setMonth] = useState(months[0]);
+    const [day, setDay] = useState(days[0]);
+    const [year, setYears] = useState(years[0]);
 
-    const changeMonth = (e: SelectChangeEvent) => {
-        setMonth(e.target.value);
+    const changeMonth = (e: SelectChangeEvent<string | number>) => {
+        if (typeof e.target.value === "string") {
+            setMonth(e.target.value);
+        }
+    };
+
+    const changeDay = (e: SelectChangeEvent<string | number>) => {
+        //there is a bug in MUI - https://github.com/mui/material-ui/issues/33399
+        if (typeof e.target.value === "number") {
+            setDay(e.target.value);
+        }
+    };
+
+    const changeYear = (e: SelectChangeEvent<string | number>) => {
+        //there is a bug in MUI - https://github.com/mui/material-ui/issues/33399
+        if (typeof e.target.value === "number") {
+            setYears(e.target.value);
+        }
     };
 
     return (
@@ -64,7 +107,12 @@ export default function SignUpForm({ handleClose, open }: Props) {
                     <TextField label="Mobile number or email" variant="outlined" size="small" fullWidth style={{ marginBottom: "10px" }} />
                     <TextField label="New password" variant="outlined" size="small" fullWidth type="password" />
                     <div css={birthday}>
-                        <LoginSelector label="Month" handleChange={changeMonth} items={months} value={month} />
+                        <div css={title}>Birthday</div>
+                        <div css={selectors}>
+                            <LoginSelector label="Month" handleChange={changeMonth} items={months} value={month} />
+                            <LoginSelector label="Day" handleChange={changeDay} items={days} value={day} />
+                            <LoginSelector label="Year" handleChange={changeYear} items={years} value={year} />
+                        </div>
                     </div>
                 </div>
             </div>
