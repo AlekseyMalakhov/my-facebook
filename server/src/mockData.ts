740685f22ff531b6c2d6a016d1839b5699f4bd0f
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-05-27 17:39:31 +0300

    server ts init

---
+++ server/src/mockData.ts
@@ -0,0 +1,22 @@
+const users = [
+    {
+        id: 1,
+        name: "Mark",
+        surname: "Mark2",
+        profession: "Prof 1",
+    },
+    {
+        id: 2,
+        name: "Paul",
+        surname: "Paul2",
+        profession: "Prof 2",
+    },
+    {
+        id: 3,
+        name: "Stieve",
+        surname: "Stieve2",
+        profession: "Prof 3",
+    },
+];
+
+export default users;
--- src/shared/api/queries/mutations.ts
+++ src/shared/api/queries/mutations.ts
@@ -1,29 +1,14 @@
 import { gql } from "@apollo/client";
 
-export const UPDATE_USER_MUTATION = gql`
-    mutation UpdateUser($input: UpdateUserInput!) {
-        updateUser(input: $input) {
-            id
-            name
-            surname
-            profession
-        }
-    }
-`;
-
-export const DELETE_USER_MUTATION = gql`
-    mutation DeleteUser($id: ID!) {
-        deleteUser(id: $id)
-    }
-`;
-
 export const ADD_USER_MUTATION = gql`
     mutation AddUser($input: AddUserInput!) {
         addUser(input: $input) {
-            id
-            name
-            surname
-            profession
+            first_name
+            last_name
+            birthday
+            gender
+            mobile_number
+            email
         }
     }
 `;
