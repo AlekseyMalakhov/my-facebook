feafb2a673102267e162ecd4c077998ac4e3c1c4
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-05-28 14:37:49 +0300

    update types

--- server/src/schema/resolvers.ts
+++ server/src/schema/resolvers.ts
@@ -1,79 +1,3 @@
-/*
-
-const resolvers = {
-    Query: {
-        //user resolvers
-        users: () => users /*here request to db should be*/,
-        //here we use default resolvers for fields of user object (name, id, surname ...)
-        /*
-        user: (parent, args) => {
-            const id = Number(args.id);
-            const user = users.find((u) => u.id === id);
-            return user;
-        },
-        */
-        //but if we want we can specify resolvers for them manually
-        /*
-        user(parent, args) {
-            return {
-                id() {
-                    return args.id;
-                },
-                name() {
-                    const id = Number(args.id);
-                    const user = users.find((u) => u.id === id);
-                    return user.name;
-                },
-                surname() {
-                    const id = Number(args.id);
-                    const user = users.find((u) => u.id === id);
-                    return user.surname;
-                },
-                profession() {
-                    const id = Number(args.id);
-                    const user = users.find((u) => u.id === id);
-                    return user.profession;
-                },
-            };
-        },
-        *//*
-    },
-
-    Mutation: {
-        addUser: (parent, args) => {
-            const u = args.input;
-            const id = Date.now();
-            const user = { id, ...u };
-            users.push(user);
-            return user;
-        },
-
-        updateUser: (parent, args) => {
-            const user = args.input;
-            const id = Number(args.input.id);
-            const updatedUser = { ...user };
-            updatedUser.id = id;
-            const newUsers = users.map((u) => {
-                if (u.id === id) {
-                    return updatedUser;
-                }
-                return u;
-            });
-            users = newUsers;
-            return user;
-        },
-
-        deleteUser: (parent, args) => {
-            const id = Number(args.id);
-            const newUsers = users.filter((u) => u.id !== id);
-            users = newUsers;
-            return id;
-        },
-    },
-};
-
-
-*/
 import users from "../mockData";
 import { Resolvers } from "../__generated__/resolvers-types";
 
@@ -96,6 +20,5 @@ const resolvers: Resolvers = {
         },
     },
 };
-
 
 export default resolvers;
