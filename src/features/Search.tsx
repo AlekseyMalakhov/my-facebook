cbda284976b2523281d445167b730662162bbbfe
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-07-16 17:51:41 +0300

    change search input style on screen size change

--- src/features/Search.tsx
+++ src/features/Search.tsx
@@ -3,6 +3,8 @@ import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
 import SearchIcon from "@mui/icons-material/Search";
 import { colors } from "@/shared/cssSettings";
+import { IconButton } from "@mui/material";
+import { Fragment } from "react";
 
 const container = css({
     marginLeft: "9px",
@@ -29,23 +31,48 @@ const container = css({
         opacity: "1",
         letterSpacing: "0px",
     },
+    "@media only screen and (max-width: 1259px)": {
+        display: "none",
+    },
+});
+
+const container2 = css({
+    marginLeft: "9px",
+    height: "40px",
+    "@media only screen and (min-width: 1260px)": {
+        display: "none",
+    },
+});
+
+const search = css({
+    backgroundColor: colors.commentBackground,
+    width: "40px",
+    height: "40px",
+    marginRight: "8px",
 });
 
 export default function Search() {
     return (
-        <div css={container}>
-            <TextField
-                InputProps={{
-                    startAdornment: (
-                        <InputAdornment position="start">
-                            <SearchIcon sx={{ fontSize: "22px" }} />
-                        </InputAdornment>
-                    ),
-                }}
-                variant="outlined"
-                placeholder="Search Facebook"
-                fullWidth
-            />
-        </div>
+        <Fragment>
+            <div css={container}>
+                <TextField
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <SearchIcon sx={{ fontSize: "22px" }} />
+                            </InputAdornment>
+                        ),
+                    }}
+                    variant="outlined"
+                    placeholder="Search Facebook"
+                    fullWidth
+                />
+            </div>
+            <div css={container2}>
+                <IconButton aria-label="menu" css={search}>
+                    <SearchIcon sx={{ fontSize: "22px" }} />
+                </IconButton>
+            </div>
+        </Fragment>
     );
 }
--- src/shared/cssSettings.ts
+++ src/shared/cssSettings.ts
@@ -3,4 +3,5 @@ export const colors = {
     secondaryIcon: "#65676B",
     primaryText: "#050505",
     primaryIcon: "#050505",
+    commentBackground: "#F0F2F5",
 };
