9ab00f8463b93c2d07df3467e1d606096b009880
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-07-19 14:38:45 +0300

    change FeedCardCommentHeader to VE

--- src/widgets/Main/MainMiddlePanel/FeedCard/FeedCardCommentHeader/FeedCardCommentHeader.css.ts
+++ src/widgets/Main/MainMiddlePanel/FeedCard/FeedCardCommentHeader/FeedCardCommentHeader.css.ts
@@ -6,3 +6,35 @@ globalStyle(`${button}.MuiButtonBase-root.MuiIconButton-root`, {
     backgroundColor: "#E4E6EB",
     padding: 6,
 });
+
+export const container = style({
+    width: "100%",
+});
+
+export const header = style({
+    minHeight: "60px",
+    display: "flex",
+    alignItems: "center",
+});
+
+export const title = style({
+    flex: 1,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+});
+
+globalStyle(`${title} h2`, {
+    fontSize: "20px",
+    fontWeight: 700,
+    lineHeight: "24px",
+});
+
+export const divider = style({
+    backgroundColor: "#CED0D4",
+    height: "1px",
+    color: "#dadde1",
+    margin: 0,
+    padding: 0,
+    border: 0,
+});
--- src/widgets/Main/MainMiddlePanel/FeedCard/FeedCardCommentHeader/FeedCardCommentHeader.tsx
+++ src/widgets/Main/MainMiddlePanel/FeedCard/FeedCardCommentHeader/FeedCardCommentHeader.tsx
@@ -1,38 +1,6 @@
-import { css } from "@emotion/react";
 import CrossCloseButton from "@/features/CrossCloseButton";
 import { colors } from "@/shared/cssSettings";
-import { button } from "./FeedCardCommentHeader.css";
-
-const container = css({
-    width: "100%",
-});
-
-const header = css({
-    minHeight: "60px",
-    display: "flex",
-    alignItems: "center",
-});
-
-const title = css({
-    flex: 1,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    "& h2": {
-        fontSize: "20px",
-        fontWeight: 700,
-        lineHeight: "24px",
-    },
-});
-
-const divider = css({
-    backgroundColor: "#CED0D4",
-    height: "1px",
-    color: "#dadde1",
-    margin: 0,
-    padding: 0,
-    border: 0,
-});
+import { button, container, header, title, divider } from "./FeedCardCommentHeader.css";
 
 type Props = {
     name: string;
@@ -41,9 +9,9 @@ type Props = {
 
 export default function FeedCardCommentHeader({ name, onClick }: Props) {
     return (
-        <div css={container}>
-            <div css={header}>
-                <div css={title}>
+        <div className={container}>
+            <div className={header}>
+                <div className={title}>
                     <h2>{name}'s Post</h2>
                 </div>
                 <CrossCloseButton
@@ -53,7 +21,7 @@ export default function FeedCardCommentHeader({ name, onClick }: Props) {
                     iconStyle={{ color: colors.primaryIcon }}
                 />
             </div>
-            <hr css={divider}></hr>
+            <hr className={divider}></hr>
         </div>
     );
 }
