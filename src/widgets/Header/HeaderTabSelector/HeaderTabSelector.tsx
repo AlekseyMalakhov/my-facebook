78f3794e3810b46e73d357787a8048b4ca3e40e1
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-07-22 11:59:28 +0300

    HeaderTabSelector to VE

--- src/widgets/Header/HeaderTabSelector/HeaderTabSelector.tsx
+++ src/widgets/Header/HeaderTabSelector/HeaderTabSelector.tsx
@@ -1,21 +1,15 @@
-import { css } from "@emotion/react";
-import HomeIcon from "../../shared/svg/HomeIcon";
-import FriendsIcon from "../../shared/svg/FriendsIcon";
-import GroupsIcon from "../../shared/svg/GroupsIcon";
+import HomeIcon from "@/shared/svg/HomeIcon";
+import FriendsIcon from "@/shared/svg/FriendsIcon";
+import GroupsIcon from "@/shared/svg/GroupsIcon";
 import { useParams } from "react-router-dom";
-import HeaderTabLink from "./HeaderTabLink/HeaderTabLink";
-
-const container = css({
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-});
+import HeaderTabLink from "../HeaderTabLink/HeaderTabLink";
+import { container } from "./HeaderTabSelector.css";
 
 export default function HeaderTabSelector() {
     const { tab } = useParams();
 
     return (
-        <div css={container}>
+        <div className={container}>
             <HeaderTabLink selected={tab === undefined} icon={HomeIcon} to="" />
             <HeaderTabLink selected={tab === "friends"} icon={FriendsIcon} to="friends" />
             <HeaderTabLink selected={tab === "groups"} icon={GroupsIcon} to="groups/feed" />
